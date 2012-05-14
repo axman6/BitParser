5b01030e6d7977eb871788ff1863137a3b7c1e0a
Alex Mason <axman6@gmail.com>
2012-05-14 15:47:24 +1000

    Changed state from [Bool] to something that allows us to extract bytes directly.

--- BitParser/Types.hs
+++ BitParser/Types.hs
@@ -4,18 +4,37 @@ module BitParser.Types where
 import Control.Monad.State
 import Control.Monad.Error
 import Control.Monad.Identity
+import qualified Data.ByteString.Lazy as BL
+import Data.Int
 
-data BitParserError = BPE [Bool] String
+data BitParserError = BPE BitParserState String 
+
+data BitParserState = BPS {currentBS :: !BL.ByteString, bitsParsed :: !Int64, bytePos :: !Int}
+    deriving Show
+
+emptyBPS = BPS BL.empty 0 0
 
 instance Show BitParserError where
     show (BPE _ str) = str
 
-type BitParserT m a = ErrorT BitParserError (StateT [Bool] m) a
+type BitParserT m a = ErrorT BitParserError (StateT BitParserState m) a
 
 type BitParser a = BitParserT Identity a
 
 instance Error BitParserError where
-    noMsg = BPE [] ""
-    strMsg s = BPE [] s
+    noMsg = BPE emptyBPS ""
+    strMsg s = BPE emptyBPS s
 
+type BitParserResult a = Either BitParserError a
+type BitParserResultState a = (Either BitParserError a, BitParserState)
+
+
+
+err :: Monad m => String -> BitParserT m a
+err str = do
+    bits <- get
+    err' bits str
+
+err' :: Monad m =>  BitParserState -> String -> BitParserT m a
+err' bits str = throwError $ BPE bits str
 
--- BitParser/Util.hs
+++ BitParser/Util.hs
@@ -4,96 +4,127 @@ import BitParser.Types
 
 import Data.Bits
 import Data.Word
+import Data.Int
 import qualified Data.ByteString.Lazy as BL
+import qualified Data.ByteString as BS
 import Control.Monad.State
 import Control.Monad.Error
 import Control.Monad.Identity
 import Control.Applicative
 
 
-getBit :: BitParser Bool
+getBit :: Monad m => BitParserT m Bool
 getBit = do
-    bits <- get
-    case bits of
-        [] -> err "getBit: ran out of bits"
-        (x:xs) ->  do
-            put xs
-            return x
+    state <- get
+    xs <- getBits 1
+    case xs of
+        [] -> err' state $ "getBit: not enough bits"
+        [x] -> return x
 
-getBits :: Int -> BitParser [Bool]
+
+getBits :: Monad m => Int64 -> BitParserT m [Bool]
 getBits n = do
-    bits <- get
-    let (xs,ys) = splitAt n bits
-    put ys
-    return xs
+    BPS bs red bpos <- get
+    let resBits = take (fromIntegral n) . drop bpos . bsToBits $ bs
+        (x,y) = divMod n 8
+        toDrop = x + if bpos == 0 then 0 else 1
+        bs' = BL.drop (fromIntegral toDrop) bs
+    put (BPS bs' (red + n) ((bpos + fromIntegral y) `mod` 8))
+    return resBits
 
-getUnsafeN :: Bits a => Int -> BitParser a
-getUnsafeN n = createFromLower n <$> getExactly n
+getUnsafeN :: (Bits a, Monad m) => Int64 -> BitParserT m a
+getUnsafeN n = createFromLower n `liftM` getExactly n
 
-getN :: Bits a => Int -> BitParser a
+getN :: (Bits a, Monad m) => Int64 -> BitParserT m a
 getN n
-    | n > bitSize x || n < 0 = err $ "getN: n > bitSize for type"
-    | otherwise = (createFromLower n <$> getExactly n) `asTypeOf` return x
+    | n > fromIntegral (bitSize x) || n < 0 = err $ "getN: n > bitSize for type"
+    | otherwise = (createFromLower n `liftM` getExactly n) `asTypeOf` return x
     where x = undefined :: Bits a => a
-
-getExactly :: Int -> BitParser [Bool]
+-- 
+getExactly :: Monad m => Int64 -> BitParserT m [Bool]
 getExactly n = do
-    bits <- get
+    state@(BPS bs red bpos) <- get
     xs <- getBits n
-    when (length xs /= n) $ err' bits $ "getExactly: could not get exactly " ++ show n ++ " bits"
+    when (fromIntegral (length xs) /= n) $ err' state $ "getExactly: could not get exactly " ++ show n ++ " bits"
     return xs
 
-match :: Bits a => a -> BitParser a
+match :: (Bits a, Monad m) => a -> BitParserT m a
 match x = do
     bits <- get
-    xs <- getExactly (bitSize x)
+    xs <- getExactly (fromIntegral $ bitSize x)
     when (xs /= xbits) $ err' bits $ "match:\nExpected: " ++ showBits xbits ++ "\nRecieved: " ++ showBits xs
     return x
     where xbits = toBits x
 
-matchHigh :: Bits a => Int -> a -> BitParser a
+matchHigh :: (Bits a, Monad m) => Int64 -> a -> BitParserT m a
 matchHigh n x
-    | n > bitSize x = err $ "matchHigh: n > bitSize x"
+    | n > fromIntegral (bitSize x) = err $ "matchHigh: n > bitSize x"
     |otherwise = do
         bits <- get
         xs <- getExactly n
         when (xs /= xbits) $ err' bits $ "matchHigh:\nExpected: " ++ showBits xbits 
                                                 ++ "\nRecieved: " ++ showBits xs
         return $ createFromUpper n xs
-    where xbits = take n $ toBits x
+    where xbits = take (fromIntegral n) $ toBits x
 
-matchHighShifted :: Bits a => Int -> a -> BitParser a
+matchHighShifted :: (Bits a, Monad m) => Int64 -> a -> BitParserT m a
 matchHighShifted n x = do
     y <- matchHigh n x
-    return (y `shiftR` (bitSize x - n))
+    return (y `shiftR` (bitSize x - fromIntegral n))
 
 
-matchLow :: Bits a => Int -> a -> BitParser a
+matchLow :: (Bits a, Monad m) => Int64 -> a -> BitParserT m a
 matchLow n x
-    | n > bitSize x = err $ "matchLow: n > bitSize x"
+    | n > fromIntegral (bitSize x) = err $ "matchLow: n > bitSize x"
     |otherwise = do
         bits <- get
         xs <- getExactly n
         when (xs /= xbits) $ err' bits $ "matchLow:\nExpected: " ++ showBits xbits 
                                                ++ "\nRecieved: " ++ showBits xs
         return $ createFromLower n xs
-    where xbits = drop (bitSize x - n) $ toBits x
+    where xbits = drop (bitSize x - fromIntegral n) $ toBits x
 
-err :: String -> BitParser a
-err str = do
-    bits <- get
-    err' bits str
 
-err' :: [Bool] -> String -> BitParser a
-err' bits str = throwError $ BPE bits str
+bitsRead :: Monad m => BitParserT m Int64
+bitsRead = gets bitsParsed
+
+getBytes' :: Monad m => Int64 -> BitParserT m BS.ByteString
+getBytes' n = do
+    xs <- getExactly (n*8)
+    return . BS.pack . toWord8s $ xs
+
+-- | Skip any remaining partial bits until the next byte in the bytestring,
+-- then get the next n bytes. This will usually be more efficient than getBytes'
+-- since it doesn't have to create a new ByteString from bits.
+-- 
+-- If you know that the bytes you're going to read are byte aligned (they don't
+-- start half way through a byte), then you should use this instead of getBytes'.
+getBytes :: Monad m => Int64 -> BitParserT m BL.ByteString
+getBytes n = do
+    state@(BPS bs red pos) <- get
+    let (s,res) = case pos of
+                    0 -> let (xs,ys) = BL.splitAt n bs
+                             s = BPS ys (red + fromIntegral pos + (n * 8)) 0
+                         in (s,xs)
+                    _ -> let (xs,ys) = BL.splitAt n (BL.drop 1 bs)
+                             s = BPS ys (red + (n * 8)) 0
+                         in (s,xs)
+    put s
+    when (BL.length res /= n) $ err' state $ "getBytes': could not get " ++ show n ++ " bytes"
+    return res
+      
+
+
+
 
 bsToBits :: BL.ByteString -> [Bool]
 bsToBits bs = BL.unpack bs >>= toBits
 
-createFromLower :: Bits a => Int -> [Bool] -> a
-createFromLower n xs = foldl (\n (p,b) -> if b then setBit n p else n) 0 $ zip [n-1,n-2..0] xs
+createFromLower :: Bits a => Int64 -> [Bool] -> a
+createFromLower n xs = foldl (\n (p,b) -> if b then setBit n p else n) 0 $ zip [n'-1,n'-2..0] xs
+    where n' = fromIntegral n
 
-createFromUpper :: Bits a => Int -> [Bool] -> a
+createFromUpper :: Bits a => Int64 -> [Bool] -> a
 createFromUpper n xs = (foldl (\n (p,b) -> if b then setBit n p else n) 0 $ zip [size,size-1..0] xs) `asTypeOf` x
     where x = undefined :: Bits a => a
           size = bitSize x - 1
@@ -101,6 +132,12 @@ createFromUpper n xs = (foldl (\n (p,b) -> if b then setBit n p else n) 0 $ zip 
 toBits :: Bits a => a -> [Bool]
 toBits n = map (testBit n) [size, size-1..0] where
     size = bitSize n - 1
+
+
+toWord8s :: [Bool] -> [Word8]
+toWord8s xs = case splitAt 8 xs of
+    (ys,[]) -> [createFromUpper 8 ys]
+    (ys,zs) -> createFromUpper 8 ys : toWord8s zs
 
 showBits :: [Bool] -> String
 showBits []         = ""
